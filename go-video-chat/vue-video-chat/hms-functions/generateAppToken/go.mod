f33d09de611f8d69a7fa03e199943c24f83f49b5
jakubpluta <plutakuba@gmail.com>
2022-10-04 12:00:36 +0200

    video chat app added. vue + go

---
+++ go-video-chat/vue-video-chat/hms-functions/generateAppToken/go.mod
@@ -0,0 +1,10 @@
+module github.com/someone/generateAppToken
+
+go 1.19
+
+require github.com/aws/aws-lambda-go v1.34.1
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
---
+++ go-video-chat/vue-video-chat/tailwind.config.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "100ms": {
+          dark: "#0b0f15",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
