56d28cbccd6fc229cf88450575c522c6a77ca7e7
jakubpluta <plutakuba@gmail.com>
2022-10-08 20:05:08 +0200

    finished user controller

--- go-restaurant-management/routes/userRouter.go
+++ go-restaurant-management/routes/userRouter.go
@@ -8,7 +8,7 @@ import (
 func UserRoutes(r *gin.Engine) {
 	r.GET("/users", controller.GetUsers())
 	r.GET("/users/:user_id", controller.GetUser())
-	r.POST("/users/signup", controller.Signup())
+	r.POST("/users/signup", controller.SignUp())
 	r.POST("/users/login", controller.Login())
 
 }
